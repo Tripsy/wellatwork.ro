7230ee146d90a5354dfcb3e4483856410b448303
Tripsy <engine@play-zone.ro>
2026-02-20 07:00:27 +0200

    fix buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -33,16 +33,15 @@ const buttonVariants = cva(
 					'hover:bg-warning/70 hover:text-warning-foreground hover:border-transparent',
 			},
 			size: {
-				xs: 'text-xs px-2 py-1.5',
-				sm: 'text-sm p-2',
-				md: 'px-4 py-2',
-				lg: 'px-8',
-				pz: 'h-11 rounded-md px-8',
+				default: 'h-10 px-4 py-2',
+				sm: 'h-9 rounded-md px-3',
+				lg: 'h-11 rounded-md px-8',
+				icon: 'h-10 w-10',
 			},
 		},
 		defaultVariants: {
 			variant: 'default',
-			size: 'md',
+			size: 'default',
 		},
 	},
 );
